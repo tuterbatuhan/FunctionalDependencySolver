1a4b9485f3029f3073eeb2897a0e890be1e6f64f
tuterbatuhan <tuterbatuhan@gmail.com>
2016-04-17 22:46:37 +0300

    niceandEasy

--- main.js
+++ main.js
@@ -7,8 +7,13 @@ function findCanonicalCovers(relationList)
 		sec.add("For Relation: "+relationList[i].name);
 		var innerSec = sec.createSection(1);
 		relationList[i].dependencyList = decompositionRule(relationList[i].dependencyList,innerSec);
+
 		relationList[i].dependencyList = removeDupp(relationList[i].dependencyList,innerSec);
-		relationList[i].dependencyList = reduce(relationList[i].dependencyList,innerSec);
+		var temp=[];
+		do{
+			temp = relationList[i].dependencyList;
+			relationList[i].dependencyList = reduce(relationList[i].dependencyList,innerSec);	
+		}while(!temp.equals(relationList[i].dependencyList));
 		innerSec.add("\nLast");
 		innerSec.add(relationList[i].dependencyList.join("\n"));
 	}
